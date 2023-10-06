c30f415d897e1f83d7775a991c273fb8c9fa1104
Marco-Mendivil-Carboni <ocrammarco01@gmail.com>
2023-10-06 19:09:37 +0200

    Rename simulate to performsim.

--- Program/test.sh
+++ Program/test.sh
@@ -19,31 +19,31 @@ mkdir $testdir
 
 echo "---"
 
-./Program/bin/simulate | grep "no arguments"
+./Program/bin/performsim | grep "no arguments"
 check $?
 
-./Program/bin/simulate 1 2 3 | grep "extra arguments"
+./Program/bin/performsim 1 2 3 | grep "extra arguments"
 check $?
 
-./Program/bin/simulate wrong-dir | grep "unable to open wrong-dir"
+./Program/bin/performsim wrong-dir | grep "unable to open wrong-dir"
 check $?
 
 echo -n > "${testdir}/adjustable-parameters.dat"
 { echo "number_of_particles -10"; echo "confinement_radius 10.00";
 } >> "${testdir}/adjustable-parameters.dat"
-./Program/bin/simulate $testdir | grep "number_of_particles out of range"
+./Program/bin/performsim $testdir | grep "number_of_particles out of range"
 check $?
 
 echo -n > "${testdir}/adjustable-parameters.dat"
 { echo "number_of_particles 512";
 } >> "${testdir}/adjustable-parameters.dat"
-./Program/bin/simulate $testdir | grep "confinement_radius out of range"
+./Program/bin/performsim $testdir | grep "confinement_radius out of range"
 check $?
 
 echo -n > "${testdir}/adjustable-parameters.dat"
 { echo "number_of_particles 512"; echo "confinement_radius 1.00";
 } >> "${testdir}/adjustable-parameters.dat"
-./Program/bin/simulate $testdir | grep "chromatin volume fraction above 0.5"
+./Program/bin/performsim $testdir | grep "chromatin volume fraction above 0.5"
 check $?
 
 echo -n > "${testdir}/adjustable-parameters.dat"
@@ -51,20 +51,20 @@ echo -n > "${testdir}/adjustable-parameters.dat"
   echo "steps_per_frame 1024";
 } >> "${testdir}/adjustable-parameters.dat"
 
-./Program/bin/simulate $testdir
+./Program/bin/performsim $testdir
 [[ -f "${testdir}/all-messages.log" && \
 -f "${testdir}/initial-condition-000.gro" && \
 -f "${testdir}/trajectory-000-000.trr" && \
 -f "${testdir}/checkpoint-000.bin" ]]
 check $?
 
-./Program/bin/simulate $testdir
+./Program/bin/performsim $testdir
 [[ -f "${testdir}/initial-condition-001.gro" && \
 -f "${testdir}/trajectory-001-000.trr" && \
 -f "${testdir}/checkpoint-001.bin" ]]
 check $?
 
-nvprof ./Program/bin/simulate $testdir 0
+nvprof ./Program/bin/performsim $testdir 0
 [[ -f "${testdir}/trajectory-000-001.trr" ]]
 check $?
 
