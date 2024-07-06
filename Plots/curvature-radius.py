dce72e5d11624fa642999c7b8327b280f17c36a0
Marco-Mendivil-Carboni <ocrammarco01@gmail.com>
2024-07-06 18:16:13 +0200

    Start organizing the report.

--- Plots/curvature-radius.py
+++ Plots/curvature-radius.py
@@ -1,4 +1,4 @@
-#Imports
+# Imports
 
 from pathlib import Path
 
@@ -7,7 +7,7 @@ import numpy as np
 import matplotlib as mpl
 from matplotlib import pyplot as plt
 
-#Set fixed parameters
+# Set fixed parameters
 
 mpl.use("pdf")
 
@@ -18,49 +18,53 @@ cm = 1 / 2.54
 mpl.rcParams["figure.figsize"] = [12.00 * cm, 8.00 * cm]
 mpl.rcParams["figure.constrained_layout.use"] = True
 
-#Define curvature radius function
+# Define curvature radius function
 
 R_o = 2
 R_n = 2 * np.sqrt(2)
 
+
 def Volume(x):
-    T_1 = 4 * R_n ** 3 * x / (1 - x)
-    T_a = 2 + np.sqrt(1 - R_o ** 2 / R_n ** 2)
-    T_b = 1 - np.sqrt(1 - R_o ** 2 / R_n ** 2)
-    T_2 = R_n ** 3 * T_a * T_b **2
+    T_1 = 4 * R_n**3 * x / (1 - x)
+    T_a = 2 + np.sqrt(1 - R_o**2 / R_n**2)
+    T_b = 1 - np.sqrt(1 - R_o**2 / R_n**2)
+    T_2 = R_n**3 * T_a * T_b**2
     return T_1 + T_2
 
+
 def Gamma(v):
-    T_1 = R_o ** 12
-    T_2 = 8 * R_o ** 6 * v ** 2
-    T_a = R_o ** 18 * v ** 2
-    T_b = 5 * R_o ** 12 * v ** 4
-    T_c = 8 * R_o ** 6 * v ** 6
-    T_d = 4 * v ** 8
+    T_1 = R_o**12
+    T_2 = 8 * R_o**6 * v**2
+    T_a = R_o**18 * v**2
+    T_b = 5 * R_o**12 * v**4
+    T_c = 8 * R_o**6 * v**6
+    T_d = 4 * v**8
     T_3 = 4 * np.sqrt(T_a + T_b + T_c + T_d)
-    T_4 = 8 * v ** 4
+    T_4 = 8 * v**4
     return T_1 + T_2 + T_3 + T_4
+
 
 def Radius(v):
     T_1 = Gamma(v) ** (1 / 3)
-    T_2 = R_o ** 4
-    T_3 = R_o ** 8 * Gamma(v) ** (- 1 / 3)
-    return (T_1 + T_2 + T_3)/(4 * v)
+    T_2 = R_o**4
+    T_3 = R_o**8 * Gamma(v) ** (-1 / 3)
+    return (T_1 + T_2 + T_3) / (4 * v)
 
-#Make curvature radius plot
+
+# Make curvature radius plot
 
 plotsrootdir = Path("Plots")
 
-fig,ax = plt.subplots()
+fig, ax = plt.subplots()
 
 ax.set_xlabel("$N'/N$")
 ax.set_ylabel("$R_c$")
 
-x = np.arange(0.0,0.5,0.001)
+x = np.arange(0.0, 0.5, 0.001)
 y = Radius(Volume(x))
 x = x[y < R_n]
 y = y[y < R_n]
 
-ax.plot(x,y,color="#d81e2c")
+ax.plot(x, y, color="#d81e2c")
 
-fig.savefig(plotsrootdir/"curvature-radius.pdf")
+fig.savefig(plotsrootdir / "curvature-radius.pdf")
--- Plots/performance.py
+++ Plots/performance.py
@@ -1,4 +1,4 @@
-#Imports
+# Imports
 
 from pathlib import Path
 
@@ -9,7 +9,7 @@ from matplotlib import pyplot as plt
 
 from io import StringIO
 
-#Set fixed parameters
+# Set fixed parameters
 
 mpl.use("pdf")
 
@@ -22,31 +22,34 @@ mpl.rcParams["figure.constrained_layout.use"] = True
 
 mpl.rcParams["legend.frameon"] = False
 
-#Load data into dataframes
+# Load data into dataframes
 
 simrootdir = Path("Simulations")
-datafilepath = simrootdir/"performance.dat"
+datafilepath = simrootdir / "performance.dat"
 
 with open(datafilepath) as datafile:
     blocklist = datafile.read().split("\n\n\n")
 
-df_1 = pd.read_csv(StringIO(blocklist[0]),
-    delim_whitespace=True,comment="#",header=None)
-df_1.columns = ["N","t_e"]
+df_1 = pd.read_csv(
+    StringIO(blocklist[0]), delim_whitespace=True, comment="#", header=None
+)
+df_1.columns = ["N", "t_e"]
 
-df_2 = pd.read_csv(StringIO(blocklist[1]),
-    delim_whitespace=True,comment="#",header=None)
-df_2.columns = ["N","t_e"]
+df_2 = pd.read_csv(
+    StringIO(blocklist[1]), delim_whitespace=True, comment="#", header=None
+)
+df_2.columns = ["N", "t_e"]
 
-df_3 = pd.read_csv(StringIO(blocklist[2]),
-    delim_whitespace=True,comment="#",header=None)
-df_3.columns = ["N","t_e"]
+df_3 = pd.read_csv(
+    StringIO(blocklist[2]), delim_whitespace=True, comment="#", header=None
+)
+df_3.columns = ["N", "t_e"]
 
-#Make performance plot
+# Make performance plot
 
 plotsrootdir = Path("Plots")
 
-fig,ax = plt.subplots()
+fig, ax = plt.subplots()
 
 ax.set_xscale("log")
 ax.set_yscale("log")
@@ -54,13 +57,10 @@ ax.set_yscale("log")
 ax.set_xlabel("$N$")
 ax.set_ylabel("$t_e$ (ms)")
 
-ax.plot(df_1["N"],df_1["t_e"],
-    marker="o",color="#d81e2c",label="GeForce 920M")
-ax.plot(df_2["N"],df_2["t_e"],
-    marker="o",color="#a31cc5",label="GeForce RTX 3050")
-ax.plot(df_3["N"],df_3["t_e"],
-    marker="o",color="#194bb2",label="RTX A4000")
+ax.plot(df_1["N"], df_1["t_e"], marker="o", color="#d81e2c", label="GeForce 920M")
+ax.plot(df_2["N"], df_2["t_e"], marker="o", color="#a31cc5", label="GeForce RTX 3050")
+ax.plot(df_3["N"], df_3["t_e"], marker="o", color="#194bb2", label="RTX A4000")
 
 ax.legend(loc="upper left")
 
-fig.savefig(plotsrootdir/"performance.pdf")
+fig.savefig(plotsrootdir / "performance.pdf")
